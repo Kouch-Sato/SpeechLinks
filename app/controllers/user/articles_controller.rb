7c9176b10f107f17fa31fe56946cb7ba1060f0c9
Kouch-Sato <redkouchred@gmail.com>
2018-06-17 20:35:15 +0900

    create article/controller

---
+++ app/controllers/user/articles_controller.rb
@@ -0,0 +1,22 @@
+class User::ArticlesController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  namespace :user do
+    resources :articles
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
