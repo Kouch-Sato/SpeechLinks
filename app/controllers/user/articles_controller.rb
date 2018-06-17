b241a5d5b0cd02d1b581745b3b06c0ac8ff46edb
Kouch-Sato <redkouchred@gmail.com>
2018-06-17 22:11:47 +0900

    記事作成機能実装

--- app/controllers/user/articles_controller.rb
+++ app/controllers/user/articles_controller.rb
@@ -1,22 +1,35 @@
 class User::ArticlesController < ApplicationController
   def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to user_articles_path
+    else 
+      render :new
+    end
   end
 
   def show
   end
 
   def index
-  end
-
-  def update
+    @articles = Article.all
   end
 
   def edit
   end
 
+  def update
+  end
+
   def destroy
   end
 
-  def create
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
