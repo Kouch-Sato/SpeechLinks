fb0b9dd6d24fbc4bc3c28f13b48c35b99efaff67
Kouch-Sato <redkouchred@gmail.com>
2018-06-23 20:41:06 +0900

    user controllerを作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_current_user, only: [:edit, :update]
+
+  def index
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      @user.save
+      redirect_to user_path(@user), notice: "プロフィールを更新しました"
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+  def ensure_current_user
+    if current_user.id != params[:id].to_i
+      redirect_to user_speeches_path, alert: "他のユーザーの情報は編集できません"
+    end
+  end  
+end
