8bd33a34eaaf569a8daa19ca29442996133521cb
Kouch-Sato <redkouchred@gmail.com>
2018-06-23 14:34:16 +0900

    Speech model作成

---
+++ db/migrate/20180623053200_create_speeches.rb
@@ -0,0 +1,10 @@
+class CreateSpeeches < ActiveRecord::Migration[5.1]
+  def change
+    create_table :speeches do |t|
+      t.string :title
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180618083309) do
+ActiveRecord::Schema.define(version: 20180623053200) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "speeches", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
