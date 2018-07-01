c4f03d7bcbfc7f37f053836020818c556e6cfa92
Kouch-Sato <redkouchred@gmail.com>
2018-07-01 17:05:31 +0900

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180623121711) do
+ActiveRecord::Schema.define(version: 20180701080223) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "user_id"
+    t.integer "speech_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "user_id"
+    t.integer "speech_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
