815c3f918647b65280de8498fb11d9599e8d53d3
Kouch-Sato <redkouchred@gmail.com>
2018-07-02 16:19:23 +0900

    likeボタンを実装

---
+++ app/controllers/user/likes_controller.rb
@@ -0,0 +1,15 @@
+class User::LikesController < ApplicationController
+  before_action :authenticate_user! 
+
+  def create
+    @like = current_user.likes.new(speech_id: params[:speech_id])
+    @like.save
+    redirect_to user_speech_path(params[:speech_id])
+  end
+
+  def destroy
+    @like = current_user.likes.find_by(speech_id: params[:speech_id])
+    @like.delete
+    redirect_to user_speech_path(params[:speech_id])
+  end
+end
