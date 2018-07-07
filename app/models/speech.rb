79182bb4d487cb19d0abdb2d3b03e25aec7f1d46
Kouch-Sato <redkouchred@gmail.com>
2018-07-08 08:22:40 +0900

    modelの整理

--- app/models/speech.rb
+++ app/models/speech.rb
@@ -1,9 +1,10 @@
 class Speech < ApplicationRecord
+  has_many :comments, dependent: :delete_all
+  has_many :likes, dependent: :delete_all
+  belongs_to :user
+
   validates :title, presence: true
   validates :content, presence: true
   validates :overview, presence: true
 
-  belongs_to :user
-  has_many :comments
-  has_many :likes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :speeches
-  has_many :comments
-  has_many :likes
+  has_many :speeches, dependent: :delete_all
+  has_many :comments, dependent: :delete_all
+  has_many :likes, dependent: :delete_all
+
+  validates :name, presence: true
+  validates :bio, presence: true
+  validates :university, presence: true
+  validates :grade, presence: true
+
+  enum university: [:東京大学, :慶應義塾大学, :聖心女子大学]
 end
