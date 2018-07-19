302f99d081504d15bcd289399f51202aeb4f1ff2
Kouch-Sato <redkouchred@gmail.com>
2018-07-19 16:26:48 +0900

    google analytics設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,9 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  # Google Analytics トラッキングID
+  GA.tracker = "UA-122375792-2"
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
