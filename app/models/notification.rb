4a14b42e5db62d5af19c1fe237c06a7a86dae950
Kouch-Sato <redkouchred@gmail.com>
2018-07-31 13:33:41 +0900

    how_long_agoを作成

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -5,4 +5,16 @@ class Notification < ApplicationRecord
 
   scope :not_read, -> { where(read: false) }
   
+  def how_long_ago
+    if (Time.now - self.created_at) < 60 * 60
+      # 1時間以内なら
+      ((Time.now - self.created_at) / 60).to_i.to_s + "分前"
+    elsif (Time.now - self.created_at) < 60 * 60 * 24
+      # 24時間以内なら
+      ((Time.now - self.created_at) / 3600).to_i.to_s + "時間前"
+    else
+      # それ以降
+      (Date.today - self.created_at.to_date).to_i.to_s + "日前"
+    end
+  end
 end
