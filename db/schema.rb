65c0d540b433359c3309914b8c4f43d9cc95af46
Kouch-Sato <redkouchred@gmail.com>
2018-07-31 13:40:18 +0900

    readのデフォルトをfalseに

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180729054236) do
+ActiveRecord::Schema.define(version: 20180731043420) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 20180729054236) do
     t.bigint "notified_by_id"
     t.integer "speech_id"
     t.string "notified_type"
-    t.boolean "read"
+    t.boolean "read", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["notified_by_id"], name: "fk_rails_ff009aac1a"
