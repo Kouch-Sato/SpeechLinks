418e5ac399cd89b9addf39d44ddd04ca73224e1b
Kouch-Sato <redkouchred@gmail.com>
2018-08-04 19:53:52 +0900

    oops

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -12,9 +12,12 @@ class Notification < ApplicationRecord
     elsif (Time.now - self.created_at) < 60 * 60 * 24
       # 24時間以内なら
       ((Time.now - self.created_at) / 3600).to_i.to_s + "時間前"
-    else
-      # それ以降
+    elsif (Date.today - self.created_at.to_date) < 30
+      # 30日以内なら
       (Date.today - self.created_at.to_date).to_i.to_s + "日前"
+    else
+      #　それ以降
+      self.created_at
     end
   end
 end
