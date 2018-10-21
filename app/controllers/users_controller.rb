559b6fb89b8eb4c17582d15777a69ad94d0d6a33
Kouch-Sato <redkouchred@gmail.com>
2018-10-21 16:43:12 +0900

    add image to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :bio, :university, :grade)
+    params.require(:user).permit(:name, :email, :bio, :university, :grade, :image)
   end
 
   def ensure_current_user
