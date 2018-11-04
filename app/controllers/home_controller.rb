6152a1293f3c5f07d355db7327ae0d1ae7fc98bf
Kouch-Sato <redkouchred@gmail.com>
2018-11-04 17:21:35 +0900

    update home top

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
   def top
-    @speeches = Speech.all.order("created_at DESC").limit(3)
+    @new_speeches = Speech.all.order("created_at DESC").limit(3)
+    @random_speeches = Speech.all.order("RAND()").limit(3)
+    @commented_speeches = Comment.all.order("created_at DESC").limit(3).map(&:speech)
+    if user_signed_in?
+      @my_speeches = current_user.speeches.order("created_at DESC").limit(3)
+      @my_liked_speeches = current_user.likes.order("created_at DESC").limit(3).map(&:speech)
+    end
+  end
+
+  def help
   end
 end
