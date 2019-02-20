75f7fcdb3ed8c9d6d5389ee1683ef8793a922549
Kouch-Sato <redkouchred@gmail.com>
2019-02-20 12:24:22 +0900

    use annotate

--- test/models/speech_test.rb
+++ test/models/speech_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: speeches
+#
+#  id         :bigint(8)        not null, primary key
+#  title      :string(255)
+#  content    :text(65535)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#  overview   :text(65535)
+#
+
 require 'test_helper'
 
 class SpeechTest < ActiveSupport::TestCase
