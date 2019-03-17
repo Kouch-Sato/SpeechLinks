a70c30f1209eaec23b74046a5cbb02d251913a1b
Kouch-Sato <redkouchred@gmail.com>
2019-03-17 11:50:30 +0900

    set default meta

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'SpeechLinks',
+      title: 'TOP',
+      reverse: true,
+      separator: '-',
+      description: 'SpeechLinksは自分の英語スピーチを共有することができるサービスです。自分の書いたスピーチを誰かに見てもらったり、日本中のスピーカーが書いたスピーチを見ることもできます。',
+      keyword: 'SpeechLinks, 英語スピーチ, ESS, speech link',
+      charaset: 'utf-8',
+      icon: [
+        { href: image_url('favicon.ico') },
+        {
+          href: image_url('apple-touch-icon.png'),
+          rel: 'apple-touch-icon'
+        }
+      ]
+    }
+  end
 end
