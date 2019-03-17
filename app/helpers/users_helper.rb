c16924e9dbd1f083f4a655e7f9a65e17765ec359
Kouch-Sato <redkouchred@gmail.com>
2019-03-17 16:06:05 +0900

    users meta tags

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,21 @@
 module UsersHelper
+  def users_edit_meta_tags
+    {
+      title: 'プロフィール編集',
+      noindex: true
+    }
+  end
+
+  def users_index_meta_tags
+    {
+      title: 'スピーカー一覧'
+    }
+  end
+
+  def users_show_meta_tags
+    {
+      title: @user.name,
+      description: @user.bio
+    }
+  end
 end
